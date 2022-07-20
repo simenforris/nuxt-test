35f01c9fe82feb8896d115876b920c8f3027d73d
Simen Førrisdal <simen.forrisdal@adresseavisen.no>
2022-07-20 15:55:50 +0200

    duplicate fresh init

---
+++ components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <p>{{count}}</p>
+      <button @click="() => setCount(count - 1)">
+        -1
+      </button>
+      <button @click="() => setCount(count + 1)">
+        +1
+      </button>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Counter',
+  props: {
+    start: {
+      type: Number,
+      required: false,
+      default: 0,
+    }
+  },
+  data() {
+    return {
+      count: this.start
+    }
+  }
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <img
+        src="/logo.svg"
+        height="100px"
+        alt="the fresh logo: a sliced lemon dripping with juice"
+      />
+      <p>
+        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
+        file, and refresh.
+      </p>
+      <Counter :start="3" />
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
